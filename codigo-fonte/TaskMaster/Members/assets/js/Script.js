668b9bb53d9b0bb5f6c1dfa2b86a2f4cd7bf33e3
Bruno Santos <110935208+BrunoSotnas@users.noreply.github.com>
2023-10-10 23:48:37 +0100

    testei varias coisas amanha termino a parte do Js

--- codigo-fonte/TaskMaster/Members/assets/js/Script.js
+++ codigo-fonte/TaskMaster/Members/assets/js/Script.js
@@ -1,1 +1,27 @@
-// alert('Welcome!')
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtém o botão de adicionar projeto e a coluna
+    const addProjectButton = document.getElementById("add-project");
+    const column = document.getElementById("column");
+
+    // Adiciona um evento de clique ao botão de adicionar projeto
+    addProjectButton.addEventListener("click", function () {
+        const cardText = prompt("Insira o título do cartão:");
+        if (cardText) {
+            const card = document.createElement("div");
+            card.className = "card";
+            card.innerText = cardText;
+
+            // Adiciona um botão de exclusão ao cartão
+            const deleteButton = document.createElement("button");
+            deleteButton.className = "delete-card";
+            deleteButton.innerText = "X";
+
+            deleteButton.addEventListener("click", function () {
+                card.remove();
+            });
+
+            card.appendChild(deleteButton);
+            column.appendChild(card);
+        }
+    });
+});
