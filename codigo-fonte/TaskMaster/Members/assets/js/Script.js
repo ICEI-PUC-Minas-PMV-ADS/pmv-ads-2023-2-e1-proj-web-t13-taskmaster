7c9689d3328a2d740dd907e6e66404dec7c987ce
Felipe van Oorshot <66143166+Fena-lander@users.noreply.github.com>
2023-10-15 19:07:08 -0300

    Backup card work version

--- codigo-fonte/TaskMaster/Members/assets/js/Script.js
+++ codigo-fonte/TaskMaster/Members/assets/js/Script.js
@@ -37,49 +37,6 @@ document.addEventListener("DOMContentLoaded", function(){
         containerCount++
     })
 })
-document.addEventListener("DOMContentLoaded", function(){
-    var addCardButton = document.getElementById('add-card');
-    var addCardRightButton = document.getElementById('add-card-right'); // Botão na coluna à direita
-    var cardContainer = document.getElementById('column'); // Selecionar a coluna onde os cartões devem ser adicionados
-    var cardCount = 1;
-    var cardCountRight = 1; // Contagem de cartões na coluna à direita
-
-    addCardButton.addEventListener("click", function(){
-        const card = document.createElement("div");
-        card.className = "card";
-
-        const cardText = prompt("Insira o título do projeto:");
-        if (cardText) {
-            card.innerText = cardText;
-
-            // Mostrar o botão "Adicionar Card" na coluna à direita
-            addCardRightButton.style.display = "block";
-        } else {
-            card.innerText = "Card " + cardCount;
-        }
-
-        cardContainer.appendChild(card);
-
-        cardCount++;
-        addCardButton.style.display = "none"; // Oculta o botão "+" após a criação do cartão
-    });
-
-    addCardRightButton.addEventListener("click", function(){
-        const card = document.createElement("div");
-        card.className = "card";
-
-        const cardText = prompt("Insira o título do cartão:");
-        if (cardText) {
-            card.innerText = cardText;
-        } else {
-            card.innerText = "Card " + cardCountRight;
-        }
-
-        // Adicionar o card na coluna à direita (implemente a lógica para a coluna à direita aqui)
-
-        cardCountRight++;
-    });
-});
 
 
 
