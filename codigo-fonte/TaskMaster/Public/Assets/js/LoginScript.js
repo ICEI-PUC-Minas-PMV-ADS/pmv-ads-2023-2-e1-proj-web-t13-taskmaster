4bcc982bfafbbe4c62b9c2f1ecab53e19da6a787
Thiago Rodrigues <tworey19@gmail.com>
2023-10-29 11:51:42 -0300

    construindo js

---
+++ codigo-fonte/TaskMaster/Public/Assets/js/LoginScript.js
@@ -0,0 +1,10 @@
+function lbtnLoginClicked(){
+
+    var Email = document.getElementById('Email').value;
+    var password= document.getElementById('password').value;
+    if(Email == "admin" && password == "adimin"){
+ }
+ else{
+    alert('usuario ou senha incorreto!');
+ }
+}
