5f3e3df660d89f80e0489cc6b93f4b03b03ea316
Thiago Rodrigues <tworey19@gmail.com>
2023-10-29 11:04:46 -0300

    linkados todas as paginas

--- codigo-fonte/TaskMaster/Public/Assets/js/WelcomeScript.js
+++ codigo-fonte/TaskMaster/Public/Assets/js/WelcomeScript.js
@@ -1,9 +1,36 @@
-var elem = document.getElementById('btnCriar uma conta');
-elem.addEventListener('click', function () {
-            alert("Criar uma conta");        
-}, false);   
-
-var elem = document.getElementById('btnLogin');
-elem.addEventListener('click', function () {
-            alert("Login");        
-}, false);   
+function eventos() {
+    //   elem.addEventListener(
+    //     "click",
+    //     function () {
+    //       //alert("Criar uma conta");
+    //     },
+    //     false
+    //   );
+    //   elem.addEventListener(
+    //     "click",
+    //     function () {
+    //       //alert("Login");
+    //     },
+    //     false
+    //   );
+  }
+  
+  function elementos() {}
+  
+  function btnSingInClicked() {
+    console.log("Botão singIn foi clicado!");
+    window.location.href = "SignIn.html";
+  }
+  
+  function btnLoginClicked() {
+    console.log("Botão Login foi clicado!");
+    window.location.href = "Login.Html";
+  }
+  
+  function init() {
+    elementos();
+    eventos();
+  }
+  
+  init();
+  
